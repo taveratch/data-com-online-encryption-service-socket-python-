f3624baa03caa4b1780be3255147bcce4a011f18
TAWEESOFT <TAWEESOFT@TAWEESOFts-MacBook-Pro.local>
2016-09-09 13:03:00 +0700

    remove duplicate function

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import hashlib
 import base64
 import time
 SERVER_PORT = 12345
-types = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
+types = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'] # type of encryption
 
 def make_connection():
     global sock,info,addr,port,in_stream,listen_sock
@@ -14,7 +14,7 @@ def make_connection():
     in_stream = sock.makefile('r') # create steam
     log("Client is connected from " + addr + " port : " + str(port))
 
-def hash_text(hashtype,text):
+def hash_text(hashtype,text): # use hashlib to hash the text
     if hashtype == '1':
         return hashlib.md5(text).hexdigest()
     elif hashtype == '2':
@@ -27,26 +27,10 @@ def hash_text(hashtype,text):
         return hashlib.sha384(text).hexdigest()
     elif hashtype == '6':
         return hashlib.sha512(text).hexdigest()
-    else:
+    else: # default is md5
         return hashlib.md5(text).hexdigest()
 
-def hash_text(hashtype,text):
-    if hashtype == '1':
-        return hashlib.md5(text).hexdigest()
-    elif hashtype == '2':
-        return hashlib.sha1(text).hexdigest()
-    elif hashtype == '3':
-        return hashlib.sha224(text).hexdigest()
-    elif hashtype == '4':
-        return hashlib.sha256(text).hexdigest()
-    elif hashtype == '5':
-        return hashlib.sha384(text).hexdigest()
-    elif hashtype == '6':
-        return hashlib.sha512(text).hexdigest()
-    else:
-        return hashlib.md5(text).hexdigest()
-
-def log(text):
+def log(text): # print log on server side
     print(text)
     print("log : timestamp : " + str(time.time()))
 
@@ -62,7 +46,7 @@ while True: # listening
         make_connection()
         continue
     msgs = msg.split(',')
-    if msgs[0] == 'CONNECTION':
+    if msgs[0] == 'CONNECTION': #send to menu to client
         sock.send('MENU|')
         sock.send('Please choose type of hash,')
         sock.send('1. md5,')
@@ -71,18 +55,18 @@ while True: # listening
         sock.send('4. sha256,')
         sock.send('5. sha384,')
         sock.send('6. sha512,\r\n')
-    elif msgs[0] in '123456' and msgs[1] == '':
+    elif msgs[0] in '123456' and msgs[1] == '': # wait for plain text
         hashtype = types[int(msgs[0])-1]
         log("log : using " + hashtype)
         sock.send('ENTER_TEXT|')
         sock.send('Please enter plain text to encrypt ('+hashtype+') : \r\n')
-    elif len(msgs) >= 2 and len(msgs[1]) > 0:
+    elif len(msgs) >= 2 and len(msgs[1]) > 0: # send encrypted text to client
         log('log : returned encrypted text')
         sock.send('ENCRYPTED|')
         sock.send('Encrypted text ('+ types[int(msgs[0])-1]+') : ')
         sock.send(str(hash_text(msgs[0],msgs[1]))+',\r\n'.encode())
-        make_connection()
-    else:
+        make_connection() # make new connection
+    else: # throw an error
         sock.send('ERROR|')
         sock.send('Please try again\r\n')
 
